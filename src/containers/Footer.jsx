f5d630a3bcb308520ac04f689e27da012a3457f6
Scott Pickering <97522229+scottpickering31@users.noreply.github.com>
2024-01-10 17:30:42 +0000

    added Footer Detailing

--- src/containers/Footer.jsx
+++ src/containers/Footer.jsx
@@ -1,7 +1,10 @@
 function Footer() {
   return (
-    <div className="bg-customBlue text-white text-center h-tiny flex flex-col justify-center items-center gap-10">
-      <h1 className="font-whisper text-8xl">Elite Drives</h1>
+    <div className="bg-customBlue text-white text-center h-tiny flex flex-col justify-center items-center">
+      <div className="flex flex-col justify-start font-whisper items-center text-8xl font-bold text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
+        <img src="src/assets/Diamond.png" className="h-2/6 w-1/4" />
+        <h1>Elite Drives</h1>
+      </div>
       <p className="text-sm">Copyright &copy; 2024. All rights reserved.</p>
     </div>
   );
