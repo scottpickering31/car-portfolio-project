3717fb77cd2ad1620b634a18ecfe686f31450a72
Scott Pickering <97522229+scottpickering31@users.noreply.github.com>
2024-01-15 12:27:30 +0000

    feat: Update tier color styles, fix typo and add support for Bronze Tier Day Rate

--- src/components/ui/Pricing/PricingCalender.jsx
+++ src/components/ui/Pricing/PricingCalender.jsx
@@ -20,6 +20,7 @@ function CalendarDisplay({
   const [dayPrice, setDayPrice] = useState(0);
   const [tarriffType, setTarriffType] = useState("");
   const [isDateSelected, setIsDateSelected] = useState(false);
+  const [tierColor, setTierColor] = useState("");
 
   // Get the car details for the selected car
   const carDetails = carMakes[selectedManufacturer].cars[selectedModel];
@@ -35,7 +36,8 @@ function CalendarDisplay({
     let newPrices = [];
     //     Calculate the price for each month
     if (days >= 31) {
-      setTarriffType("Gold Tier Achieved!");
+      setTarriffType("Gold Tier Day Rate");
+      setTierColor("#D4AF37");
       newPrices = Array.from({ length: days }, (_, index) => ({
         date: new Date(startDate.getTime() + index * oneDay)
           .toISOString()
@@ -44,7 +46,8 @@ function CalendarDisplay({
       }));
       // Calculate the price for each week
     } else if (days >= 7) {
-      setTarriffType("Silver Tier Achieved!");
+      setTarriffType("Silver Tier Day Rate");
+      setTierColor("#C0C0C0");
       newPrices = Array.from({ length: days }, (_, index) => ({
         date: new Date(startDate.getTime() + index * oneDay)
           .toISOString()
@@ -52,8 +55,9 @@ function CalendarDisplay({
         price: carDetails.weekRateBreakDown() * (index + 1),
       }));
       // Calculate the price for each day
-    } else {
-      setTarriffType("Bronze Tier Achieved!");
+    } else if (days >= 0) {
+      setTarriffType("Bronze Tier Day Rate");
+      setTierColor("#CD7F32");
       newPrices = Array.from({ length: days }, (_, index) => ({
         date: new Date(startDate.getTime() + index * oneDay)
           .toISOString()
@@ -95,16 +99,19 @@ function CalendarDisplay({
             <div className="p-5 bg-customBlue text-white font-semibold tracking-widest rounded-lg w-full shadow-2xl mobile:text-lg mobile:p-5 mobilelg:text-lg mobilelg:p-5 tablet:text-lg tablet:p-5 laptop:text-sm laptop:p-0 desktop:text-lg desktop:p-3">
               <p>Cost for chosen Rental Period:</p>
               <div className="flex flex-col justify-center gap-2 items-center">
-                <p className="font-bold bg-customBlue-lighter w-11/12 rounded-full p-2 mobile:text-xl mobilelg:text-xl tablet:text-2xl laptop:text-xl desktop:text-xl">
+                <p
+                  className="font-bold w-11/12 rounded-full p-2 mobile:text-xl mobilelg:text-xl tablet:text-2xl laptop:text-xl desktop:text-xl"
+                  style={{ backgroundColor: tierColor }}
+                >
                   £{total}
                 </p>
                 <p>
-                  (£{Math.ceil(dayPrice)} Per day, {tarriffType})
+                  (£{Math.ceil(dayPrice)} Per day. {tarriffType})
                 </p>
               </div>
             </div>
             <img
-              className="hover:scale-110 transition duration-300 cursor-pointer w-2/3 mobile:h-full mobile:p-5 mobilelg:h-1/2 mobilelg:p-5 tablet:p-2 tablet:h-1/2 laptop:p-2 laptop:h-1/2 desktop:p-1 desktop:h-1/2 widescreen:w-1/4"
+              className="hover:scale-110 w-full transition duration-300 cursor-pointer mobile:h-full mobile:p-5 mobilelg:h-1/2 mobilelg:p-5 tablet:p-2 tablet:h-1/2 laptop:p-2 laptop:h-1/2 desktop:p-1 desktop:h-1/2 widescreen:w-3/4"
               src={carDetails.image}
             />
             <div className="flex flex-row items-center mobile:w-full gap-10 mobile:justify-center mobile:p-5 mobilelg:w-full mobilelg:p-5 mobilelg:justify-center tablet:w-auto tablet:p-5 tablet:justify-end laptop:w-auto laptop:p-0 laptop:justify-end desktop:p-0 desktop:w-auto desktop:justify-end">
